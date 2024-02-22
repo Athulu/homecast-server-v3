fae6f3767ed2fae4b8e297b3bac5a4ea060aeda3
Athulu <nikodem1703@gmail.com>
2024-02-23 00:04:32 +0100

    initialize project

---
+++ src/main/java/com/example/homecastserverv3/HomecastServerV3Application.java
@@ -0,0 +1,13 @@
+package com.example.homecastserverv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomecastServerV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomecastServerV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homecastserverv3/HomecastServerV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homecastserverv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomecastServerV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
