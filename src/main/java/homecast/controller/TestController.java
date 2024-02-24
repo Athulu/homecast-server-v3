b50022a224efad5788c21743f7ef47a7f789582a
Athulu <nikodem1703@gmail.com>
2024-02-25 00:29:46 +0100

    annotation corrected

--- src/main/java/homecast/controller/TestController.java
+++ src/main/java/homecast/controller/TestController.java
@@ -1,12 +1,11 @@
-package homecast;
+package homecast.controller;
 
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.stereotype.Controller
-@RequestMapping("/endpoint")
-@AllArgsConstructor
+@RestController
+@RequestMapping("/")
 public class TestController {
 
     @GetMapping("/test")
