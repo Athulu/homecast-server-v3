fb72b8d2d121d09b61dfcdc3fbd3f97cfee5116c
Athulu <nikodem1703@gmail.com>
2024-05-09 23:01:38 +0200

    add simple test

--- src/test/java/homecast/HomecastServerV3ApplicationTests.java
+++ src/test/java/homecast/HomecastServerV3ApplicationTests.java
@@ -3,11 +3,23 @@ package homecast;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 class HomecastServerV3ApplicationTests {
 
     @Test
     void contextLoads() {
+        // given
+        int a = 1;
+        int b = 1;
+
+        // when
+        int result = a + b;
+
+        // then
+        assertEquals(2, result);
     }
 
 }
