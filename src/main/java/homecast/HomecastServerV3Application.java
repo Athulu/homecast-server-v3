16caa3384a73057236858b70138c9a282b90b731
Athulu <nikodem1703@gmail.com>
2024-05-09 23:01:12 +0200

    refactor

--- src/main/java/homecast/HomecastServerV3Application.java
+++ src/main/java/homecast/HomecastServerV3Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HomecastServerV3Application {
-
     public static void main(String[] args) {
         SpringApplication.run(HomecastServerV3Application.class, args);
     }
