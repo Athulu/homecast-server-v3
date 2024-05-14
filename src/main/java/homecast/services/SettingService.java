4e16e53a0660df3db5011f9dacd91152336903e7
Athulu <nikodem1703@gmail.com>
2024-05-14 23:16:00 +0200

    update SettingService

--- src/main/java/homecast/services/SettingService.java
+++ src/main/java/homecast/services/SettingService.java
@@ -14,16 +14,9 @@ import java.util.HashSet;
 @AllArgsConstructor
 public class SettingService {
     private static final Logger LOG = LoggerFactory.getLogger(SettingService.class);
-    private final EntityManager entityManager;
     private final SettingRepository settingRepository;
-    private static HashSet<Setting> settingHashSet = new HashSet<>();
 
-    public void initializeSettings(){
-        LOG.info("Initializing settings");
-        settingHashSet = new HashSet<>(settingRepository.findAll());
-    }
-
-    public static HashSet<Setting> getSettingHashSet(){
-        return settingHashSet;
+    public HashSet<Setting> getAllSettings() {
+        return new HashSet<>(settingRepository.findAll());
     }
 }
