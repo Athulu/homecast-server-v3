b250e8668790f849c2382697bf4819aa54d88b6a
Athulu <nikodem1703@gmail.com>
2025-01-01 22:59:11 +0100

    trigger need ddl-auto to be set on "update" to correctly add trigger from migrate script

--- src/main/resources/db/migration/V003__change_setting_trigger.sql
+++ src/main/resources/db/migration/V003__change_setting_trigger.sql
@@ -3,4 +3,4 @@ DROP TRIGGER IF EXISTS setting_change_notify_trigger ON public.setting;
 CREATE TRIGGER setting_change_notify_trigger
     AFTER INSERT OR UPDATE OR DELETE
     ON public.setting
-    FOR EACH ROW EXECUTE FUNCTION setting_change_notify();
+    FOR EACH ROW EXECUTE FUNCTION public.setting_change_notify();
